43f0676f799f1bd4e6b28104690a4da7d60c1a7b
Pobphut <phopphut@gmail.com>
2026-02-24 14:22:06 +0700

    Complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(int agrc,char* agrv[]){
+    double a = 0;
+    if(agrc == 1) cout << "Please input numbers to find average.";
+    else{
+        double s = agrc - 1;
+        cout << "---------------------------------\n";
+        for(int i = 1 ;i < agrc;i++){
+            a += atof(agrv[i]); 
+        }
+        cout << "Average of " << agrc - 1 << " numbers = " << a / s << endl;
+        cout << "---------------------------------\n";
+    }
+
+}
